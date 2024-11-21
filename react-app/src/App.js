2dcd721944134441d0027c21ec5623b12f5cdf0b
El Samp <anthony.francisco.aoct@gmail.com>
2024-11-21 12:50:55 -0500

    pages

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Login from "./pages/login";
-
+import Register from "./pages/register"; // Import the Register page
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/*" 
-          element={<Layout />}
-        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/*" element={<Layout />} />
       </Routes>
     </Router>
   );
